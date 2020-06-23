a3577a4f0bf4cfd257a7d600545d61a10e0a6b19
Shannon Makenna <shannonkariuki@gmail.com>
2020-06-23 14:36:34 -0400

    created App.css and header component

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Header(){
+	return(
+		<div>
+		<header id="header" class="grid"> 
+			<div class ="bg-image"></div>
+			<div class = "content-wrap">
+				<h1>STEAM LAB </h1>
+			</div>
+		</header>
+		</div>
+		)
+}
+
+export default Header;
