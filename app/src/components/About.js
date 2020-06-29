c8d7fd99f273d0252c7160f9dfdc4215003f0755
PaulineGitau <paulinegitau31@gmail.com>
2020-06-29 08:52:41 -0400

    add About

---
+++ app/src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Grid, Cell} from 'react-mdl';
+
+function About(){
+	return(
+		<div>
+		
+		<hr/>
+		<div className="about-paragraph">
+		<p style = {{width: '100%', margin: 'auto', paddingTop: 'lem', position: 'absolute', left: '50%', top: '50%',
+        transform: 'translate(-50%, -50%)', justifyContent: "center", alignItems: "center"}}>STEAM-LAB is Science and Technology centered initatieve that aims at increasing the 
+		level of technology skills among kids in kenya. We offer offer learning support
+		services with a keen focuses in Science, Technology, Engineering, Arts and Mathematics. bla bla bla 
+		STEAM-LAB is Science and Technology centered initatieve that aims at increasing the 
+		level of technology skills among kids in kenya. We offer offer learning support
+		services with a keen focuses in Science, Technology, Engineering, Arts and Mathematics. bla bla bla</p>
+		</div> 
+		</div>
+	);	
+}
+
+export default About;
