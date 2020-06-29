b43597138f5beb6fb59c5ab540e0a749c1835b2e
Shannon Makenna <shannonkariuki@gmail.com>
2020-06-29 12:26:33 -0400

    added landing page pic

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -4,10 +4,17 @@ import React from 'react';
 function Header(){
 	return(
 		<div>
+		<nav id="nav-bar">
+			<a href= "#about">About</a>
+			<a href ="#tracks">Tracks</a>
+			<a href="#contact">Contact Us </a>
+		</nav>
 		<header id="header" class="grid"> 
-			<div class ="bg-image"></div>
-			<div class = "content-wrap">
-				<h1>STEAM LAB </h1>
+			<div className ="bg-image">
+					<div className = "content-wrap">
+					 <h1 id="title-name"> STEAM LAB </h1>
+					</div>
+
 			</div>
 		</header>
 		</div>
