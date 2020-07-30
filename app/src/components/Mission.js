4564c189805671dbc6013088d0fbfb46bcc823be
PaulineGitau <paulinegitau31@gmail.com>
2020-07-30 18:28:02 -0400

    fix

--- app/src/components/Mission.js
+++ app/src/components/Mission.js
@@ -7,10 +7,10 @@ function Mission(){
 			<div id= "bigtext"> 
 				<div className ="biiiig-image">
 		     		<h1> </h1>
-		     		<h3>The STEAM LAB KE Community is dedicated to support  young people in our network get the most our of their
+		     		<h4>The STEAM LAB KE Community is dedicated to support  young people in our network get the most our of their
 		     		STEM and Arts education or interests. We are a growing community with a platform to grow with each other from 
 		     		different levels. There are various opportunities for skills development and chart personal career paths with 
-		     		the help of a strong network of similarly visioned people</h3>
+		     		the help of a strong network of similarly visioned people</h4>
 		     	</div>			 
 			</div>
 		</div>
